6b948185e302bfaca71b3c77145dde7d03d4d382
zolangi <zolangiramirez@gmail.com>
2017-12-01 01:49:44 +0000

    Update urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.IndexView,as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
